d31593bd055369cd5aec856769a6601ba0f7df72
Savy92 <>
2023-06-07 22:49:57 +0000

    declare const for DOM elements & possible choices

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+/** 
+ * Declare constants for Dom elements
+ * and possible choices
+ */
+const buttons = document.getElementsByClassName("option-image");
+const game = document.getElementsByClassName("match-container");
+const playerScore = document.getElementById("player");
+const computerScore = document.getElementById("computer");
+const playerImage = docment.getElementsByClassName("player-hand");
+const computerImage = document.getElementsByClassName("computer-hand");
+const result = document.getElementsByClassName("result");
+const choices = ["rock", "paper", "sicssors"];
