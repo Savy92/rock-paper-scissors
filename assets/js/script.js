39c603fc2bb7dba2173db09232a8226e82ef7b44
Savy92 <>
2023-06-08 00:54:18 +0000

    add event listener to new game button

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,11 +2,24 @@
  * Declare constants for Dom elements
  * and possible choices
  */
-const buttons = document.getElementsByClassName("option-image");
-const game = document.getElementsByClassName("match-container");
+const optionButtons = document.getElementsByClassName("option-image");
 const playerScore = document.getElementById("player");
 const computerScore = document.getElementById("computer");
 const playerImage = docment.getElementsByClassName("player-hand");
 const computerImage = document.getElementsByClassName("computer-hand");
 const result = document.getElementsByClassName("result");
+const start = document.getElementById("start-game");
+const scoreBoard = document.getElementById("score-board");
+const help = document.getElementById("help");
+const introScreen = document.getElementsByClassName("intro-page");
+const gameScreen = document.getElementsByClassName("match-container");
 const choices = ["rock", "paper", "sicssors"];
+
+let Pscore = 0;
+let cScore = 0;
+
+function startGame() {
+    start.addEventListener("click", function () => {
+        introScreen.classList.add("fadeOut");
+    });
+};
