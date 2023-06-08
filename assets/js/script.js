488ebed3ee9aae95aac7f2b0d13204ba70529f70
Savy92 <>
2023-06-08 14:42:44 +0000

    add play match functions which produce a result based off the event listener

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,12 @@
-const game = () => {
-    let pScore = 0;
-    let cScore = 0;
+// Computer options
+const computerChoice = ["rock", "paper", "scissors"];
 
-    // Start the game
+let pScore = 0;
+let cScore = 0;
+
+const game = () => {
+
+    // Start the game, once the 'New Game button is clicked
     const startGame = () => {
         const start = document.querySelector('.intro-page button');
         const introScreen = document.querySelector('.intro-page');
@@ -13,16 +17,58 @@ const game = () => {
             matchScreen.classList.add('fadeIn');
         });
     };
-    // Play match
+    // Play match, once the player selects an option
     const playMatch = () => {
         const optionButtons = document.querySelectorAll('.options button');
         const playerHand = document.querySelector | ('.player-hand');
         const computerHand = document.querySelector | ('.computer-hand');
+        const result = document.querySelector('.result');
 
-        // Computer options
-        const choices = ["rock", "paper", "sicssors"];
+        optionButtons.forEach((button) => {
+            button.addEventListener('click', function () {
+                // Computer result
+                const randomNumber = Math.floor(Math.random() * 3);
+                const computerResult = computerChoice[randomNumber];
 
-        let computerChoice = Math.floor(Math.random() * 3);
+            });
+        });
+
+        const compareSelection = (playerResult, computerResult) => {
+            if (playerResult === computerResult) {
+                result.textContent = 'Draw!';
+                return;
+            }
+
+            if (playerResult === 'rock') {
+                if (computerResult === 'scissors') {
+                    result.textContent = 'You Win!';
+                    return;
+                } else {
+                    result.textContent = 'You lost this round!';
+                    return;
+                }
+            }
+
+            if (playerResult === 'paper') {
+                if (computerResult === 'rock') {
+                    result.textContent = 'You Win!';
+                    return;
+                } else {
+                    result.textContent = 'You lost this round!';
+                    return;
+                }
+            }
+
+            if (playerResult === 'scissors') {
+                if (computerResult === 'paper') {
+                    result.textContent = 'You Win!';
+                    return;
+                } else {
+                    result.textContent = 'You lost this round!';
+                    return;
+                }
+            }
+        };
     };
 
     startGame();
