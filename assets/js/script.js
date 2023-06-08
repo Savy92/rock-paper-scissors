5949affed6438e732d3ee1a9438b53ee8a9ae6c9
Savy92 <>
2023-06-08 02:46:54 +0000

    add js functionsto start the game and play the game

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,32 +1,32 @@
-/** 
- * Declare constants for Dom elements
- * and possible choices
- */
-const optionButtons = document.getElementsByClassName("option-image");
-const playerScore = document.getElementById("player");
-const computerScore = document.getElementById("computer");
-const playerImage = docment.getElementsByClassName("player-hand");
-const computerImage = document.getElementsByClassName("computer-hand");
-const result = document.getElementsByClassName("result");
-const start = document.getElementById("start-game");
-const scoreBoard = document.getElementById("score-board");
-const help = document.getElementById("help");
-const introScreen = document.getElementsByClassName("intro-page");
-const gameScreen = document.getElementsByClassName("match-container");
-const choices = ["rock", "paper", "sicssors"];
+const game = () => {
+    let pScore = 0;
+    let cScore = 0;
 
+    // Start the game
+    const startGame = () => {
+        const start = document.querySelector('.intro-page button');
+        const introScreen = document.querySelector('.intro-page');
+        const matchScreen = document.querySelector('.match-container');
 
+        start.addEventListener('click', () => {
+            introScreen.classList.add('fadeOut');
+            matchScreen.classList.add('fadeIn');
+        });
+    };
+    // Play match
+    const playMatch = () => {
+        const optionButtons = document.querySelectorAll('.options button');
+        const playerHand = document.querySelector | ('.player-hand');
+        const computerHand = document.querySelector | ('.computer-hand');
 
-let pscore = 0;
-let cScore = 0;
+        // Computer options
+        const choices = ["rock", "paper", "sicssors"];
 
-/**
- * Add a click event listener to new game button
- */
-//Start the game
-const startGame = () => {
-    start.addEventListener("click", () => {
-        introScreen.classList.add("fadeOut");
-        gameScreen.classList.add("fadeIn");
-    });
+        let computerChoice = Math.floor(Math.random() * 3);
+    };
+
+    startGame();
+    playMatch();
 };
+
+game();
